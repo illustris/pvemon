14db1fa68c5db4d78d54ff6f82b1378661edfb79
illustris <rharikrishnan95@gmail.com>
2024-09-09 01:02:02 +0530

    Bump version to 1.2.0

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pvemon',
-    version = "1.1.6",
+    version = "1.2.0",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
